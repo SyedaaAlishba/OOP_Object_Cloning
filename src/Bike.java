25aa7c8e46dedfc050bb4be5e2ead723990495c6
wajahat <wajahatlisyed@gmail.com>
2025-07-13 20:20:55 +0500

    first commit

---
+++ src/Bike.java
@@ -0,0 +1,28 @@
+
+public class Bike extends Vehical {
+    public  Bike (String name, String model, String company, String color){
+        this.name =name;
+        this.color= color;
+        this.model=model;
+        this.company=company;
+    }
+
+
+    public Bike(Bike other){
+        this.name =other.name;
+        this.color= other.color;
+        this.model=other.model;
+        this.company=other.company;
+    }
+
+    public void print(){
+        System.out.println(name);
+        System.out.println(color);
+        System.out.println(model);
+        System.out.println(company);
+
+    }
+
+
+
+}
---
+++ src/Vehical.java
@@ -0,0 +1,11 @@
+public class Vehical {
+    String name="";
+    String color="";
+    String model="";
+    String company="";
+
+    void Start_Engine(){
+        System.out.println("Starting engine");
+    }
+
+}
